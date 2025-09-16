43b9b8dd969dec972c2c79a57d5de1b61376f3e4
EMLonergan <emlonergan92@gmail.com>
2025-09-16 15:23:06 +0200

    Reduce click rate to 60

--- LoadTrialDependentWaveform.m
+++ LoadTrialDependentWaveform.m
@@ -29,7 +29,7 @@ if ~BpodSystem.EmulatorMode
         CurrentAudBias = TaskParameters.GUI.BlockTable.AudLeftBias(BlockTableMask);
 
         % Generate click trains with block bias
-        TotalClicks = 100;
+        TotalClicks = 60;
         [LeftClickTrain, RightClickTrain] = GetClickStimulus(iTrial, TaskParameters.GUI.AuditoryStimulusTime, fs, ClickLength, SoundLevel, 'biasedUniform', CurrentAudBias, TotalClicks);
 
         % Load waveforms
